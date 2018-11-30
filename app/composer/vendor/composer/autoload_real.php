51c6fba9adc76018258f5f427447a2f37493d9e1
Alonsovz <fabiomejiash@gmail.com>
2018-11-30 10:54:50 -0600

    mpdf

--- app/composer/vendor/composer/autoload_real.php
+++ app/composer/vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit1cce736b1a3607e10d9ca8d49315b841
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit1cce736b1a3607e10d9ca8d49315b841::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire1cce736b1a3607e10d9ca8d49315b841($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire1cce736b1a3607e10d9ca8d49315b841($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
