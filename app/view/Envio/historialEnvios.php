4364cc87836964a4a3f92dde42c18c03ef9eba83
Alonsovz <fabiomejiash@gmail.com>
2018-11-30 18:09:31 -0600

    Historial

---
+++ app/view/Envio/historialEnvios.php
@@ -0,0 +1,91 @@
+<div id="app">
+
+    <modal-detalles :detalles="detalles"></modal-detalles>
+
+   
+
+    <div class="ui grid">
+        <div class="row">
+            <div class="titulo">
+                <i class="box icon"></i>
+                Historial de Envíos<font color="#85BC22" size="20px">.</font>
+            </div>
+        </div>
+        <br><br><br>
+
+
+        <div class="row">
+            <div class="sixteen wide column">
+                <table id="dtHistorial" class="ui selectable very compact celled table" style="width:100%; margin:auto;">
+                    <thead>
+                        <tr>
+                            <th>N°</th>
+                            <th>Fecha</th>
+                            <th>Hora</th>
+                            <th>Usuario</th>
+                            <th>Nombre</th>
+                            <th>Documentos</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="./res/tablas/tablaHistorial.js"></script>
+<script src="./res/js/modalHistorialD.js"></script>
+
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+            detalles: []
+        },
+        methods: {
+            cargarDetalles(id) {
+
+                $('#frmDetalles').addClass('loading');
+                $.ajax({
+                    type: 'POST',
+                    url: '?1=EnvioController&2=getDetallesEnvioH',
+                    data: {
+                        id: id
+                    },
+                    success: function (data) {
+                        app.detalles = JSON.parse(data);
+                        $('#frmDetalles').removeClass('loading');
+                    }
+                });
+            },
+
+            cerrarModal() {
+                this.detalles = [];
+            },
+
+            modalCambiar() {
+                $('#modalCambios').modal('setting', 'autofocus', false).modal('setting', 'closable', false)
+                .modal('show');
+                $('#modalDetalles').modal('hide');
+            },
+
+            cambiarEstado() {
+                $('#modalDetalles').modal('setting', 'autofocus', false).modal('setting', 'closable', false)
+                .modal('show');
+                $('#modalCambios').modal('hide');
+            }
+        }
+    });
+</script>
+
+<script>
+    $(function () {
+        $(document).on("click", ".btnVer", function () {
+            $('#modalDetalles').modal('setting', 'autofocus', false).modal('setting', 'closable', false)
+                .modal('show');
+            app.cargarDetalles($(this).attr('id'));
+        });
+    });
+</script>
---
+++ res/js/modalHistorialD.js
@@ -0,0 +1,72 @@
+Vue.component('modal-detalles', {
+
+    props: {
+        detalles: {
+            type: Array,
+            required: false
+        }
+    },
+
+
+    template: `<div class="ui longer fullscreen first coupled modal" id="modalDetalles">
+                    <div class="header">
+                        Documentos del Paquete
+                    </div>
+
+                    <div class="scrolling content">
+                    <form action="" class="ui form" id="frmDetalles">
+                        <table v-if="detalles" class="ui selectable very compact single line table">
+                        <thead>
+                            <tr>
+                                <th>Trámite</th>
+                                <th>Cliente</th>
+                                <th>Área</th>
+                                <th>Tipo de Documento</th>
+                                <th>N° Documento</th>
+                                <th>Monto</th>
+                                <th>Status</th>
+                                <th>Observación</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="detalle in detalles" :codigo-detalle="detalle.codigoDetalleEnvio">
+                                <td>{{detalle.descTipoTramite}}</td>
+                                <td>{{detalle.nombreCliente}}</td>
+                                <td>{{detalle.descArea}}</td>
+                                <td>{{detalle.descTipoDocumento}}</td>
+                                <td>{{detalle.numDoc}}</td>
+                                <td>{{detalle.monto}}</td>
+
+                                
+                                <td  v-if="detalle.descStatus == 'Pendiente'" style="background-color: #F6AD43;">
+                                    {{detalle.descStatus}}
+                                </td>
+                                <td  v-if="detalle.descStatus == 'Revisado'" style="background-color: #F67943;">
+                                    {{detalle.descStatus}}
+                                </td>
+                                <td  v-if="detalle.descStatus == 'Completo'" style="background-color: lightgreen;">
+                                    {{detalle.descStatus}}
+                                </td>
+                                <td  v-if="detalle.descStatus == 'Regresado a Finanzas'" style="background-color: lightblue;">
+                                    {{detalle.descStatus}}
+                                </td>
+                                
+
+                                <td>
+                                    {{detalle.observacion}}
+                                </td>
+                                
+                            </tr>
+                        </tbody>
+                        </table>
+                        </form>
+                    </div>
+                    <div class="actions">
+                        <button @click="$parent.cerrarModal" class="ui deny button">
+                            Cerrar
+                        </button>
+                    </div>
+                </div>`
+
+});
