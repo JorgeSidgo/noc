6064586eaafc1878efe37c6ef1660717f7074b68
Alonsovz <fabiomejiash@gmail.com>
2018-12-09 21:01:48 -0600

    ProcedimientosGraficas

--- db/script.sql
+++ db/script.sql
@@ -491,7 +491,8 @@ create procedure clientesConMasEnvios()
 begin
 select count(c.codigoCliente) as Cliente, c.nombreCliente  from detalleEnvio d
 inner join clientes c on c.codigoCliente = d.codigoCliente
-group by c.nombreCliente;
+inner join envio e on e.codigoEnvio = d.codigoEnvio
+ where e.fecha between (SELECT date_add(CURDATE(), INTERVAL -7 DAY)) and CURDATE() group by c.nombreCliente;
 end
 $$
 
@@ -501,7 +502,7 @@ create procedure usuariosEnvios()
 begin
 select count(c.codigoUsuario) as Usuario, c.nomUsuario  from envio e
 inner join usuario c on c.codigoUsuario = e.codigoUsuario
- group by c.nomUsuario;
+ where e.fecha between (SELECT date_add(CURDATE(), INTERVAL -7 DAY)) and CURDATE() group by c.nomUsuario;
 end
 $$
 
@@ -687,7 +688,6 @@ insert into tipoDocumento values(null, 'Otro');
 #Status 
 insert into status values (null, 'Pendiente');
 insert into status values (null, 'Revisado');
-insert into status values (null, 'Recibido');
 insert into status values (null, 'Completo');
 insert into status values (null, 'Regresado a Finanzas');
 
@@ -700,9 +700,9 @@ insert into detalleEnvio values (null, 'DD3', 1, 1, 3, 1, 1, 1, '123', '$1', 'na
 insert into detalleEnvio values (null, 'DD4', 1, 1, 1, 1, 1, 4, '123', '$1', 'nada');
 
 
-select * from envio;
+-- select * from detalleEnvio;
 
-call detallesEnvio(2);
+-- call mostrarPaquetes;
 
-call getEncabezadoEnvio(2)
-call mostrarPaquetes;
+select * from usuario;
+
